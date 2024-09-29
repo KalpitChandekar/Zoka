cf61a6686e249f6c4349a183a1dedba809b36bc0
Kalpit <kalpitchandekar1736@gmail.com>
2024-09-29 13:25:28 +0530

    [feat]: create Hero section

---
+++ components/Hero.js
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import Button from "./Button";
+
+const Hero = () => {
+  return (
+    <div className="flex my-20 max-w-6xl mx-auto">
+      <div className="mx-auto ">
+        <h1 className="text-7xl font-bold">Where creative process happens</h1>
+        <p className="my-8">
+          Your creativity, our inspiration <br /> Whatever your story, set if
+          free.
+        </p>
+        <Button />
+      </div>
+      <div className="w-1/2 flex justify-end">
+        <Image
+          src="/hero.png"
+          width={670}
+          height={680}
+          alt="hero"
+          className="w-[28rem]"
+        />
+      </div>
+    </div>
+  );
+};
+export default Hero;
