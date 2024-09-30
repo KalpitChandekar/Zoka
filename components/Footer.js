e9e17e8c42033a1bc73351e437b3bc773a64c739
Kalpit <kalpitchandekar1736@gmail.com>
2024-09-30 14:06:50 +0530

    create Footer

---
+++ components/Footer.js
@@ -0,0 +1,22 @@
+const Footer = () => {
+  return (
+    <div className="max-w-5xl mx-auto flex mb-10 justify-between items-center font-semibold text-[#3C3842]">
+      <h1>ZOKA © 2024. Crafted by YEV YEV ❤️ </h1>
+      <ul className="flex gap-6">
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/">Pages</a>
+        </li>
+        <li>
+          <a href="/">Photography</a>
+        </li>
+        <li>
+          <a href="/">Photoshoots</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+export default Footer;
