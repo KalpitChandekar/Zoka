73410cfc58492f742e403eecad485a934343c388
Kalpit <kalpitchandekar1736@gmail.com>
2024-09-30 13:42:10 +0530

    create get in touch button

---
+++ components/WhiteButton.js
@@ -0,0 +1,11 @@
+import { CircleChevronRight } from "lucide-react";
+
+const WhiteButton = () => {
+  return (
+    <div className="flex gap-2 items-center bg-white justify-center rounded-full py-3 text-secondary w-44 font-semibold">
+      <h1>Get in Touch</h1>
+      <CircleChevronRight size={20} />
+    </div>
+  );
+}
+export default WhiteButton
